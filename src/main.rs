a2bf3cdb4852d984fa11b930dc1c85aff1203530
4sito <andreas.arayaosorio@outlook.com>
2025-04-25 11:02:40 +0200

    utilizzo json

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::io;
+use std::fs;
 
 fn main(){
-    println!("Che parte della storia vuoi? A o B?");
-    
+    // println!("Che parte della storia vuoi? A o B?");
+    let story = fs::read_to_string("src/story.json").expect("Unable to read file");
+    //println!("{}", story);
+    let json: serde_json::Value =
+        serde_json::from_str(story.as_str()).expect("Couldn't Parse JSON");
+
+    //println!("{}", json);
+    println!("{}", json["0"]);
     let mut input = String::new();
 
     io::stdin()
@@ -10,10 +17,10 @@ fn main(){
         .expect("Failed to read line");
 
     
-    println!("You chose: {}!", input.trim());
+    //println!("You chose: {}!", input.trim());
     match input.trim() {
-        "A"=>println!("AAAAAAA"),
-        "B"=>println!("BBBB, la storia andra avanti con l'opzione b"),
+        "A"=>println!("{}", json["1"]),
+        "B"=>println!("{}", json["2"]),
         _=>println!("opzione non valida"),
     }
 }
